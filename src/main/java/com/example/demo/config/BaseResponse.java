4c793b1029b7c8a152f5ee1e6deda96516c09d7f
mieczy <mieczy@naver.com>
2022-09-20 23:00:31 +0900

    Feat: Validation 개선
    1. 데이터 유효성 검증 에러 메세지 개선
    2. BaseResponseStatus
       - 응답코드 스타일 변경

--- src/main/java/com/example/demo/config/BaseResponse.java
+++ src/main/java/com/example/demo/config/BaseResponse.java
@@ -1,13 +1,10 @@
 package com.example.demo.config;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.List;
 
 import static com.example.demo.config.BaseResponseStatus.*;
 
@@ -22,14 +19,13 @@ public class BaseResponse<T> {
     private final String message;
     private final int code;
     private T result;
-    private List<validError> error;
 
 
-    // GET 요청에 성공한 경우
+    // 요청에 성공한 경우
     public BaseResponse(T result) {
         this.isSuccess = SUCCESS_WITH_DATA.isSuccess();
-        this.message = SUCCESS_WITH_DATA.getMessage();
         this.code = SUCCESS_WITH_DATA.getCode();
+        this.message = SUCCESS_WITH_DATA.getMessage();
         this.result = result;
     }
 
@@ -37,26 +33,17 @@ public class BaseResponse<T> {
     // 요청에 실패한 경우
     public BaseResponse(BaseResponseStatus status) {
         this.isSuccess = status.isSuccess();
-        this.message = status.getMessage();
         this.code = status.getCode();
+        this.message = status.getMessage();
     }
 
 
     // Validation 실패한 경우
-    public BaseResponse(List<validError> error){
-        this.isSuccess = INVALID_REQUEST.isSuccess();
-        this.message = INVALID_REQUEST.getMessage();
-        this.code = INVALID_REQUEST.getCode();
-        this.error = error;
+    public BaseResponse(BaseResponseStatus status, String message){
+        this.isSuccess = status.isSuccess();
+        this.message = message;
+        this.code = status.getCode();
     }
 
-
-    // Validation 실패 에러 내역
-    @Getter
-    @AllArgsConstructor
-    public static class validError{
-        private String field;
-        private String errMessage;
-    }
 }
 
