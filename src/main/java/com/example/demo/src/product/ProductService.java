d55e959e4b9862eb4d99f5736e42f28b78f4de49
geonho <soo_1992@naver.com>
2022-09-22 14:06:33 +0900

    Feat: 상품 등록 API @Valid를 통한 형식적 validation 추가

--- src/main/java/com/example/demo/src/product/ProductService.java
+++ src/main/java/com/example/demo/src/product/ProductService.java
@@ -5,7 +5,6 @@ package com.example.demo.src.product;
 import com.example.demo.config.BaseException;
 import com.example.demo.src.product.model.*;
 import com.example.demo.utils.JwtService;
-import com.example.demo.utils.SHA256;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
