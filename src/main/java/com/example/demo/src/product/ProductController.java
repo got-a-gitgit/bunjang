432f93e7deb44155b779139d3d6fb6b1a7c30ffb
geonho <soo_1992@naver.com>
2022-09-22 23:17:37 +0900

    Feat: 관심 상품 삭제 API 추가

--- src/main/java/com/example/demo/src/product/ProductController.java
+++ src/main/java/com/example/demo/src/product/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 
--- src/main/java/com/example/demo/src/wish/WishProvider.java
+++ src/main/java/com/example/demo/src/wish/WishProvider.java
@@ -1,2 +1,31 @@
-package com.example.demo.src.wish;public class WishProvider {
+package com.example.demo.src.wish;
+
+
+import com.example.demo.utils.JwtService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import static com.example.demo.config.BaseResponseStatus.*;
+
+
+@Service
+public class WishProvider {
+
+    private final WishDao wishDao;
+    private final JwtService jwtService;
+
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public WishProvider(WishDao wishDao, JwtService jwtService) {
+        this.wishDao = wishDao;
+        this.jwtService = jwtService;
+    }
+
+
 }
