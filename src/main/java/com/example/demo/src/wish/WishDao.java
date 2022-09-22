02b48b0017acb6ff6ecfeac80ddbefc2ef1e4c31
geonho <soo_1992@naver.com>
2022-09-22 23:17:37 +0900

    Feat: 관심 상품 삭제 API 추가

--- src/main/java/com/example/demo/src/wish/WishDao.java
+++ src/main/java/com/example/demo/src/wish/WishDao.java
@@ -1,2 +1,33 @@
-package com.example.demo.src.wish;public class WishDao {
+package com.example.demo.src.wish;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+
+@Repository
+public class WishDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+    public void createWish(int userId, int productId) {
+        String query = "INSERT INTO wish(user_id,product_id) VALUES(?, ?) ON DUPLICATE KEY UPDATE status= 'Y'";
+        Object[] params = new Object[]{userId, productId};
+        this.jdbcTemplate.update(query, params);
+    }
+
+    public void deleteWish(int userId, int productId) {
+        String query = "UPDATE wish SET status = 'N' WHERE user_id=? AND product_id=? AND status='Y'";
+        Object[] params = new Object[]{userId, productId};
+        this.jdbcTemplate.update(query, params);
+    }
 }
