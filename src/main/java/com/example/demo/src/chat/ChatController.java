17da7cb85c330385ef24e99238dc954931cbdef0
geonho <soo_1992@naver.com>
2022-09-26 02:53:05 +0900

    Feat: 채팅 시작하기&메세지 보내기 API 구현

---
+++ src/main/java/com/example/demo/src/chat/ChatController.java
@@ -0,0 +1,50 @@
+package com.example.demo.src.chat;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.chat.model.PostSendMessageReq;
+import com.example.demo.utils.JwtService;
+import com.example.demo.utils.S3Service;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/chats")
+public class ChatController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final ChatProvider chatProvider;
+    @Autowired
+    private final ChatService chatService;
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final S3Service s3Service;
+
+    /**
+     * 채팅 시작하기 & 메세지 보내기 API
+     * [POST] /chats
+     * @return BaseResponse
+     */
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse sendMessage(@RequestBody PostSendMessageReq postSendMessageReq) throws BaseException {
+        //jwt 인증
+        int userId= jwtService.getUserId();
+
+        try{
+            chatService.sendMessage(userId, postSendMessageReq);
+            return new BaseResponse<>(SUCCESS_WITH_DATA);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/src/chat/ChatDao.java
@@ -0,0 +1,61 @@
+package com.example.demo.src.chat;
+
+import com.example.demo.src.chat.model.PostSendMessageReq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class ChatDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+    public int checkChatroom(int userId, int productId) {
+        String query = "SELECT c.room_id as room_id\n" +
+                "FROM chatroom c\n" +
+                "JOIN user_chatroom uc on c.room_id = uc.room_id\n" +
+                "WHERE uc.user_id=? AND c.product_id=?";
+
+        Object[] params = new Object[]{userId, productId};
+
+        try {
+            return jdbcTemplate.queryForObject(query, Integer.class, params);
+        } catch (Exception exception) {
+            return -1;
+        }
+    }
+
+    public int sendMessage(int userId, int chatroomId, PostSendMessageReq postSendMessageReq) {
+        String query = "INSERT INTO chat_message(room_id, user_id, message, message_type) VALUES(?,?,?,?)";
+        Object[] params = new Object[]{chatroomId, userId, postSendMessageReq.getMessage(), postSendMessageReq.getMessageType()};
+
+        return jdbcTemplate.update(query, params);
+    }
+
+    public int createChatroom(int productId) {
+        String query = "INSERT INTO chatroom(product_id) VALUE (?)";
+        jdbcTemplate.update(query, productId);
+
+        String lastInserIdQuery = "select last_insert_id()";
+        return this.jdbcTemplate.queryForObject(lastInserIdQuery,int.class);
+    }
+
+    public void joinChatroom(int userId, int chatroomId) {
+        String query = "INSERT INTO user_chatroom(user_id, room_id) VALUES (?,?)";
+        Object[] params = new Object[]{userId, chatroomId};
+
+        jdbcTemplate.update(query, params);
+    }
+
+    public void activateChatroom(int chatroomId) {
+        String query ="UPDATE user_chatroom SET status='Y' WHERE room_id=?";
+        jdbcTemplate.update(query, chatroomId);
+    }
+}
