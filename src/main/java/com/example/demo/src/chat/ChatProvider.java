75cae5e04e12efc98859dc4fba1217d5646683c6
geonho <soo_1992@naver.com>
2022-09-26 02:53:05 +0900

    Feat: 채팅 시작하기&메세지 보내기 API 구현

---
+++ src/main/java/com/example/demo/src/chat/ChatProvider.java
@@ -0,0 +1,38 @@
+package com.example.demo.src.chat;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
+
+@Service
+public class ChatProvider {
+    private final ChatDao eventDao;
+    private final JwtService jwtService;
+
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public ChatProvider(ChatDao eventDao, JwtService jwtService) {
+        this.eventDao = eventDao;
+        this.jwtService = jwtService;
+    }
+
+
+
+
+    /** Event list 조회 **/
+//    public List<GetEventListRes> getEventList() throws BaseException {
+//        try {
+//            //홈 화면 이벤트 리스트 조회
+//            return eventDao.getEventList();
+//        } catch (Exception exception) {
+//            throw new BaseException(DATABASE_ERROR);
+//        }
+//    }
+}
