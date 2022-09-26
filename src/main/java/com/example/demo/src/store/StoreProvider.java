0630a57fd1ebe58c53f462ec6804bd5315144d36
mieczy <mieczy@naver.com>
2022-09-26 10:41:03 +0900

    Feat: 본인인증 정보 조회 API 추가

--- src/main/java/com/example/demo/src/store/StoreProvider.java
+++ src/main/java/com/example/demo/src/store/StoreProvider.java
@@ -27,10 +27,10 @@ public class StoreProvider {
     }
 
     /** 등록된 유저인지 확인 **/
-    public int checkUserId(int userId) throws BaseException{
+    public int checkUserId(int userId) throws BaseException {
         try {
             return storeDao.selectUserId(userId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("CheckUserId Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
@@ -40,7 +40,7 @@ public class StoreProvider {
     public int checkDuplicatesStoreName(int userId, String name) throws BaseException {
         try {
             return storeDao.selectStoreName(userId, name);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("CheckDuplicatesStoreName Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
@@ -50,7 +50,7 @@ public class StoreProvider {
     public PatchStoreProfileRes getStoreProfile(int userid) throws BaseException {
         try {
             return storeDao.selectStoreProfile(userid);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetStoreProfile Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
@@ -66,7 +66,7 @@ public class StoreProvider {
 
         try {
             return storeDao.selectStoreInfo(storeId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetStoreInfo Error", e);
             throw new BaseException(FAIL_GET_STOREINFO);
         }
@@ -75,13 +75,13 @@ public class StoreProvider {
     /** 상점의 거래내역(판매) 조회 **/
     public List<TradeInfo> getSales(int userId, String type) throws BaseException {
         // 전체 조회 시
-        if (type.equals("All")){
+        if (type.equals("All")) {
             type = "t.status";
         }
 
         try {
             return storeDao.selectSales(userId, type);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetSales", e);
             throw new BaseException(FAIL_GET_SALES);
         }
@@ -90,13 +90,13 @@ public class StoreProvider {
     /** 상점의 거래내역(구매) 조회 **/
     public List<TradeInfo> getPurchases(int userId, String type) throws BaseException {
         // 전체 조회 시
-        if (type.equals("All")){
+        if (type.equals("All")) {
             type = "t.status";
         }
 
         try {
             return storeDao.selectPurchases(userId, type);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetPurchases", e);
             throw new BaseException(FAIL_GET_PURCHASE);
         }
@@ -106,7 +106,7 @@ public class StoreProvider {
     public int checkFollowing(int userId, int storeId) throws BaseException {
         try {
             return storeDao.selectIsFollwing(userId, storeId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("checkFollowing", e);
             throw new BaseException(DATABASE_ERROR);
         }
@@ -128,9 +128,9 @@ public class StoreProvider {
             if (followInfo.size() != size + 1) {
                 hasNextPage = false;
             }
-            if (hasNextPage){
+            if (hasNextPage) {
                 followInfo.remove(size);  // 마지막 데이터 삭제
-                lastFollowId = followInfo.get(size-1).getUserId();       // 마지막 팔로워 Id
+                lastFollowId = followInfo.get(size - 1).getUserId();       // 마지막 팔로워 Id
             }
 
             return new GetFollowRes(followInfo, hasNextPage, lastFollowId);
@@ -151,7 +151,7 @@ public class StoreProvider {
 
         try {
             List<followInfo> followInfo = storeDao.selectFollowings(storeId, lastId, size);
-            for (followInfo store : followInfo){
+            for (followInfo store : followInfo) {
                 List<ProductInfo> products = storeDao.selectProductsByStore(store.getUserId());
                 store.setProductInfoList(products);
             }
@@ -162,9 +162,9 @@ public class StoreProvider {
             if (followInfo.size() != size + 1) {
                 hasNextPage = false;
             }
-            if (hasNextPage){
+            if (hasNextPage) {
                 followInfo.remove(size);  // 마지막 데이터 삭제
-                lastFollowId = followInfo.get(size-1).getUserId();       // 마지막 팔로잉 Id
+                lastFollowId = followInfo.get(size - 1).getUserId();       // 마지막 팔로잉 Id
             }
 
             return new GetFollowRes(followInfo, hasNextPage, lastFollowId);
@@ -195,40 +195,40 @@ public class StoreProvider {
     }
 
     /** 기본 계좌 ID 조회 **/
-    public int getMainAccount(int userId) throws BaseException{
+    public int getMainAccount(int userId) throws BaseException {
         try {
             return storeDao.selectMainAccount(userId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetMainAccount Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
     }
 
     /** 일반 계좌 ID 조회 **/
-    public int getNotMainAccount(int userId) throws BaseException{
+    public int getNotMainAccount(int userId) throws BaseException {
         try {
             return storeDao.selectNotMainAccount(userId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetNotMainAccount Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
     }
 
     /** 계좌 번호 조회 **/
-    public String getAccountNumber(int accountId) throws BaseException{
+    public String getAccountNumber(int accountId) throws BaseException {
         try {
             return storeDao.selectAccountNumber(accountId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetAccountNumber Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
     }
 
     /** 계좌 유효성 확인 **/
-    public int getAvailableAccount(int userId, int accountId) throws BaseException{
+    public int getAvailableAccount(int userId, int accountId) throws BaseException {
         try {
             return storeDao.selectAccountExist(userId, accountId);
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetAvailableAccount Error", e);
             throw new BaseException(DATABASE_ERROR);
         }
@@ -238,12 +238,27 @@ public class StoreProvider {
     public List<AccountInfo> getAccounts(int userId) throws BaseException {
         try {
             return storeDao.selectAccounts(userId);
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("GetAccounts Error", e);
             throw new BaseException(FAIL_GET_ACCOUNTS);
         }
     }
 
+    /** 본인인증 정보 조회 **/
+    public GetAuthenticationRes getUserAuthentication(int storeId) throws BaseException {
+        // 유효한 유저인지 확인
+        int isUser = checkUserId(storeId);
+        if (isUser == 0) {
+            throw new BaseException(INVALID_ACCESS);
+        }
+        try {
+            return storeDao.selectUserAuthentication(storeId);
+        } catch (Exception e) {
+            logger.error("GetAuthentications Error", e);
+            throw new BaseException(FAIL_GET_USER_AUTHENTICATION);
+        }
+    }
+}
     // 페이징 처리
 //    /** 상점의 거래내역(판매) 조회 **/
 //    public GetSalesRes getSales(int userId, int tradeId, String date, int size) throws BaseException {
@@ -269,4 +284,4 @@ public class StoreProvider {
 //            throw new BaseException(FAIL_GET_SALES);
 //        }
 //    }
-}
+
