b99753f87fae6b08a31ec8fc38d4074af25bb1b3
geonho <soo_1992@naver.com>
2022-09-26 21:24:39 +0900

    Feat : 채팅방 상세조회 API

--- src/main/java/com/example/demo/src/chat/ChatDao.java
+++ src/main/java/com/example/demo/src/chat/ChatDao.java
@@ -1,11 +1,15 @@
 package com.example.demo.src.chat;
 
+import com.example.demo.src.chat.model.GetChatroomListRes;
+import com.example.demo.src.chat.model.GetChatroomRes;
+import com.example.demo.src.chat.model.Message;
 import com.example.demo.src.chat.model.PostSendMessageReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class ChatDao {
@@ -63,5 +67,76 @@ public class ChatDao {
         String query ="UPDATE user_chatroom SET status='N' WHERE user_id=? AND room_id=?";
         Object[] params = new Object[]{userId, chatroomId};
         jdbcTemplate.update(query, params);
+    }
+
+    public List<GetChatroomListRes> getChatroomList(int userId) {
+        String query = "SELECT c.room_id     as room_id,\n" +
+                "       s.profile_image_url,\n" +
+                "       s.store_name  as store_name,\n" +
+                "       cm.updated_at as last_updated_at,\n" +
+                "       cm.message    as last_sent_message,\n" +
+                "       pi.url        as product_image_url\n" +
+                "FROM chatroom c\n" +
+                "         JOIN user_chatroom uc on c.room_id = uc.room_id\n" +
+                "         JOIN store s on uc.user_id = s.user_id\n" +
+                "         LEFT JOIN (SELECT product_id, MIN(product_image_id), url\n" +
+                "                    FROM product_image pi\n" +
+                "                    GROUP BY product_id) pi on c.product_id = pi.product_id\n" +
+                "         JOIN (SELECT room_id, message, MAX(updated_at) as updated_at\n" +
+                "               FROM chat_message\n" +
+                "               GROUP BY room_id) cm on c.room_id = cm.room_id\n" +
+                "WHERE uc.user_id != ?\n" +
+                "  AND c.room_id in (SELECT room_id\n" +
+                "                    FROM user_chatroom\n" +
+                "                    WHERE user_id = ?\n" +
+                "                      AND status = 'Y')\n";
+        Object[] params = new Object[]{userId, userId};
+
+        return this.jdbcTemplate.query(query,
+                (rs, rowNum) -> new GetChatroomListRes(
+                        rs.getInt("room_id"),
+                        rs.getString("profile_image_url"),
+                        rs.getString("store_name"),
+                        rs.getString("last_updated_at"),
+                        rs.getString("last_sent_message"),
+                        rs.getString("product_image_url")
+                ), params);
+    }
+
+    public GetChatroomRes getChatroom(int userId, int chatroomId) {
+        String query = "SELECT s.store_name, s.user_id, p.product_id, p.name, p.price, s.profile_image_url\n" +
+                "FROM store s\n" +
+                "         JOIN user_chatroom uc on s.user_id = uc.user_id\n" +
+                "         JOIN chatroom c on uc.room_id = c.room_id\n" +
+                "         JOIN product p on c.product_id = p.product_id\n" +
+                "WHERE s.user_id = ?\n" +
+                "  AND uc.room_id = ?";
+
+        Object[] params = new Object[]{userId, chatroomId};
+
+        return jdbcTemplate.queryForObject(query,
+                (rs,rowNum)->new GetChatroomRes(
+                        rs.getString("store_name"),
+                        rs.getInt("user_id"),
+                        rs.getInt("product_id"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getString("profile_image_url")
+                ),params);
+    }
+
+    public List<Message> getMessageList(int chatroomId) {
+        String query = "SELECT user_id, message, message_type, updated_at as written_at\n" +
+                "FROM chat_message\n" +
+                "WHERE room_id=?";
+
+
+        return jdbcTemplate.query(query,
+                (rs,rowNum)->new Message(
+                        rs.getInt("user_id"),
+                        rs.getString("message"),
+                        rs.getString("message_type"),
+                        rs.getString("written_at")
+                ),chatroomId);
     }
 }
